94c4fbdf405a8c2b6176e410011581f63e129173
Nenad Vracar <nenadwebdevelop@gmail.com>
2020-07-24 17:15:24 +0200

    updated structure

---
+++ client/src/components/App/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+describe('When I render App component', () => {
+    it('works fine', () => {
+        expect(1).toBe(1);
+    })
+})
